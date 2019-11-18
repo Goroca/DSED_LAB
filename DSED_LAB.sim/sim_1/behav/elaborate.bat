3e20ef0f5c510e9fc2aae208de8d9467892db6be
alejandroramosmartin <alexramartin@gmail.com>
2019-11-18 23:37:00 +0100

    modulo1.4_version1

--- DSED_LAB.sim/sim_1/behav/elaborate.bat
+++ DSED_LAB.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto a9b082810ecd4554a74c2f800bd0f577 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot FSMD_microphone_tb1_behav xil_defaultlib.FSMD_microphone_tb1 -log elaborate.log
+call %xv_path%/xelab  -wto a9b082810ecd4554a74c2f800bd0f577 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot en_4_cycles_tb_behav xil_defaultlib.en_4_cycles_tb -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
--- DSED_LAB.srcs/sources_1/new/audio_interface.vhd
+++ DSED_LAB.srcs/sources_1/new/audio_interface.vhd
@@ -22,41 +22,89 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 use work.package_dsed.all;
 
 entity audio_interface is
-Port ( clk_12megas : in STD_LOGIC;
-reset : in STD_LOGIC;
---Recording ports
---To/From the controller
-record_enable: in STD_LOGIC;
-sample_out: out STD_LOGIC_VECTOR (sample_size-1 downto 0);
-sample_out_ready: out STD_LOGIC;
---To/From the microphone
-micro_clk : out STD_LOGIC;
-micro_data : in STD_LOGIC;
-micro_LR : out STD_LOGIC;
---Playing ports
---To/From the controller
-play_enable: in STD_LOGIC;
-sample_in: in std_logic_vector(sample_size-1 downto 0);
-sample_request: out std_logic;
---To/From the mini-jack
-jack_sd : out STD_LOGIC;
-jack_pwm : out STD_LOGIC);
+    Port ( clk_12megas : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           --Recording ports
+           --To/From the controller
+           record_enable: in STD_LOGIC;
+           sample_out: out STD_LOGIC_VECTOR (sample_size-1 downto 0);
+           sample_out_ready: out STD_LOGIC;
+           --To/From the microphone
+           micro_clk : out STD_LOGIC;
+           micro_data : in STD_LOGIC;
+           micro_LR : out STD_LOGIC;
+           --Playing ports
+           --To/From the controller
+           play_enable: in STD_LOGIC;
+           sample_in: in std_logic_vector(sample_size-1 downto 0);
+           sample_request: out std_logic;
+           --To/From the mini-jack
+           jack_sd : out STD_LOGIC;
+           jack_pwm : out STD_LOGIC);
 end audio_interface;
 
 architecture Behavioral of audio_interface is
 
+component en_4_cycles
+    Port ( clk_12megas : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           clk_3megas : out STD_LOGIC;
+           en_2_ciclos : out STD_LOGIC;
+           en_4_ciclos : out STD_LOGIC);
+end component;
+
+component FSMD_microphone
+    Port ( clk_12megas : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           enable_4_cycles : in STD_LOGIC;
+           micro_data : in STD_LOGIC;
+           sample_out : out STD_LOGIC_VECTOR (sample_size-1 downto 0);
+           sample_out_ready : out STD_LOGIC);
+end component;
+
+component pwm
+    Port(  clk_12megas: in std_logic;
+           reset: in std_logic;
+           en_2_cycles: in std_logic;
+           sample_in: in std_logic_vector(sample_size-1 downto 0);
+           sample_request: out std_logic;
+           pwm_pulse: out std_logic);
+end component;
+
+-- señales auxiliares
+signal aux_clk_3megas : STD_LOGIC;
+signal aux_en_2_ciclos, aux_en_4_ciclos : STD_LOGIC;
+
 begin
 
+-- Unities declaration
+U1: en_4_cycles
+    port map(
+        clk_12megas => clk_12megas,
+        reset => reset,
+        clk_3megas => aux_clk_3megas,
+        en_2_ciclos => aux_en_2_ciclos,
+        en_4_ciclos => aux_en_4_ciclos);
+        
+U2: FSMD_microphone
+    port map(
+        clk_12megas => clk_12megas,
+        reset => reset,
+        enable_4_cycles => aux_en_4_ciclos,
+        micro_data => micro_data,
+        sample_out => sample_out, 
+        sample_out_ready => sample_out_ready);
+
+U3: pwm
+    port map(
+        clk_12megas => clk_12megas,
+        reset => reset,
+        en_2_cycles => aux_en_2_ciclos,
+        sample_in => sample_in, 
+        sample_request => sample_request,
+        pwm_pulse => jack_pwm);
 
 end Behavioral;
