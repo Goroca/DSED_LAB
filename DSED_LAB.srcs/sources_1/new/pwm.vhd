9f2c4c27069cd1ff7579b869dc9d70239de61e35
Goroca <gorocarlos98@gmail.com>
2019-11-19 10:26:17 +0100

    Modulo 1.3 version 2

--- DSED_LAB.srcs/sources_1/new/pwm.vhd
+++ DSED_LAB.srcs/sources_1/new/pwm.vhd
@@ -25,7 +25,7 @@ use work.package_dsed.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -33,9 +33,8 @@ use work.package_dsed.all;
 --use UNISIM.VComponents.all;
 
 entity pwm is
-port(
-clk_12megas, in std_logic;
-reset, in std_logic;
+port(clk_12megas: in std_logic;
+reset: in std_logic;
 en_2_cycles: in std_logic;
 sample_in: in std_logic_vector(sample_size-1 downto 0);
 sample_request: out std_logic;
@@ -45,8 +44,29 @@ end pwm;
 
 
 architecture Behavioral of pwm is
+signal r_reg : unsigned (sample_size-1 downto 0);
+signal r_next : unsigned (sample_size-1 downto 0);
+signal buff:  std_logic;
+signal next_buff:  std_logic;
 
 begin
 
+process(clk_12megas,reset)
+begin
+    if (reset ='1') then
+        buff<='0';
+        r_reg<= (others=>'0');
+    elsif(rising_edge(clk_12megas)) then
+        buff<=next_buff;
+        r_reg<= r_next;
+    end if;
+end process;
+r_next<=r_reg+1;
+
+next_buff <= 
+    '1' when (r_reg<unsigned(sample_in) or sample_in="0000000") else
+    '0';
+    
+pwm_pulse<= buff;
 
 end Behavioral;
