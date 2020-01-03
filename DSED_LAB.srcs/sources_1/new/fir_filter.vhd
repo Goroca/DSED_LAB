7b4d35248df1fbfd4cb1b3a5bd4bf284e7611357
Goroca <gorocarlos98@gmail.com>
2020-01-03 01:20:53 +0100

    Añadir duplicado

--- DSED_LAB.srcs/sources_1/new/fir_filter.vhd
+++ DSED_LAB.srcs/sources_1/new/fir_filter.vhd
@@ -44,7 +44,150 @@ end fir_filter;
 
 architecture Behavioral of fir_filter is
 
+signal aux_Sample_Out_ready : STD_LOGIC ;
+
+signal windows : STD_LOGIC ;
+signal count : unsigned (2 downto 0) := (others=> '0');
+
+signal c0,c1,c2,c3,c4 : signed (sample_size-1 downto 0);
+signal x0,x1,x2,x3,x4 : signed (sample_size-1 downto 0);
+signal next_x0,next_x1,next_x2,next_x3,next_x4 : signed (sample_size-1 downto 0);
+
+signal next_R1 :signed (sample_size-1 downto 0);
+signal R1 :signed (sample_size-1 downto 0);
+signal next_R2 :signed (sample_size-1 downto 0);
+signal R2 :signed (sample_size-1 downto 0);
+signal next_R3 :signed (sample_size-1 downto 0);
+signal R3 :signed (sample_size-1 downto 0);
+
+signal mul_in0 :signed (sample_size-1 downto 0);
+signal mul_in1 :signed (sample_size-1 downto 0);
+signal mul_out :signed (15 downto 0);
+
+
+signal sum_in0 :signed (sample_size-1 downto 0);
+signal sum_in1 :signed (sample_size-1 downto 0);
+signal sum_out :signed (sample_size-1 downto 0);
+
 begin
 
+Count_process: process (clk)
+begin
+ if (Reset = '1') then
+   count <= (others=>'0');
+  elsif(clk'event and clk=SAMPLE_CLK_EDGE and windows = '1') then
+    count<= count +1;
+end if;
+end process;
+Sample_process: process (clk,Reset)
+begin
+ if (Reset = '1') then
+    x0 <= (others=>'0');
+    x1 <= (others=>'0');
+    x2 <= (others=>'0');
+    x3 <= (others=>'0');
+    x4 <= (others=>'0');
 
+ elsif (clk'event and clk=SAMPLE_CLK_EDGE ) then
+    if (Sample_In_enable='1') then
+        windows <= '1';
+        x0 <= next_x0;
+        x1 <= next_x1;
+        x2 <= next_x2;
+        x3 <= next_x3;
+        x4 <= next_x4;    
+    elsif(count = "101") then
+        aux_Sample_Out_ready <= '1';
+        windows <= '0';
+     else
+        aux_Sample_Out_ready <= '0';
+    end if;    
+ end if;
+end process;
+
+next_x4 <= x3;
+next_x3 <= x2;
+next_x2 <= x1;
+next_x1 <= x0;
+next_x0 <= Sample_In;
+
+process(filter_select)
+begin
+if (filter_select='0') then
+    c0 <= "00000101";
+    c4 <= "00000101";
+    c1 <= "00011111";
+    c3 <= "00011111";
+    c2 <= "00111001";
+else
+    c0 <= "11111111";
+    c4 <= "11111111";
+    c1 <= "11100110";
+    c3 <= "11100110";
+    c2 <= "01001101";
+end if;
+
+end process;
+
+process (clk,Reset)
+begin
+    if (Reset = '1') then
+        R1 <= (others=>'0');
+        R2 <= (others=>'0');
+        R3 <= (others=>'0');
+
+    elsif (clk'event and clk=SAMPLE_CLK_EDGE and windows ='1') then
+        R1 <= next_R1;
+        R2 <= next_R2;
+        R3 <= next_R3;
+    end if;
+    
+end process;
+
+sum_out <= sum_in0 + sum_in1;
+mul_out <= to_signed ( to_integer(mul_in0) * to_integer(mul_in1) , mul_out'length);
+R3 <= mul_out (7 + sample_size - 1  downto sample_size-1); 
+
+with count(1 downto 0) select mul_in0 <=
+	C2 when "00",
+	C0 when "01",
+	C1 when "10",
+	(others=>'0') when others;
+	
+with count(1 downto 0) select mul_in1 <=
+    x2 when "00",
+    R2 when "01",
+    R2 when "10",
+    (others=>'0') when others;
+        
+with count select sum_in0 <=
+    x0 when "000",
+    x1 when "001",
+    R1 when "011",
+    R1 when "100",
+    (others=>'0') when others;
+        
+with count select sum_in1 <=
+    x4 when "000",
+    x3 when "001",
+    R2 when "011",
+    R2 when "100",
+    (others=>'0') when others;
+        
+with count select next_R2 <=
+    sum_out when "000",
+    sum_out when "001",
+    R3      when "010",
+    sum_out when "100",
+    sum_out when "101",
+    (others=>'0') when others;
+        
+with count(0 downto 0) select next_R1 <=
+    R1 when "0",
+    sum_out when "1",
+    (others=>'0') when others;
+
+Sample_out <= R2;
+Sample_Out_ready <=aux_Sample_Out_ready;                      
+                                                          
 end Behavioral;
