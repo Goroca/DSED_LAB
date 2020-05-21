268ba397390c2a3df2085aafce2444b50c36c3e2
Gorocarlos <gorocarlos98@gmail.com>
2020-05-21 20:49:25 +0200

    Cambios en git ignore

--- DSED_LAB.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto a9b082810ecd4554a74c2f800bd0f577 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip -L xpm --snapshot DSED_TB_behav xil_defaultlib.DSED_TB -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- DSED_LAB.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim DSED_TB_behav -key {Behavioral:sim_1:Functional:DSED_TB} -tclbatch DSED_TB.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
